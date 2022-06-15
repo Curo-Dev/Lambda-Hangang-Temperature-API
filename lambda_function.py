099095ca80361a8f2242b36db6d7d9f0123f3f42
Curo-Dev <admin@curo.xyz>
2022-06-15 14:55:42 +0900

    feat: updated code

--- lambda_function.py
+++ lambda_function.py
@@ -1,47 +1,37 @@
-import datetime
+from datetime import timedelta, datetime
 import requests
 from bs4 import BeautifulSoup
 from pytz import timezone
 
+dt = datetime.now(timezone("Asia/Seoul"))
+now = dt.strftime('%Y/%m/%d %H:%M:%S')
+year = dt.strftime('%Y')
+month = dt.strftime('%m')
+day = (dt - timedelta(days=1)).strftime('%d')
+dateFormat = year +'-' + month + '-' + str(day)
+date = year + month + day
+
+def getTemp():
 
 
-def getDate(dt):
-  return dt.strftime('%Y-%m-%d')
+  return data.json()['list'][0]
 
-def getTemp(date):
-  url = "http://water.nier.go.kr/web/waterMeasure/toastList"
+def lambda_handler(event, context):
+  url = "http://water.nier.go.kr/web/autoMeasure/noConfirm/toastList"
 
   params = {
-    'dFielNm': '자료 조회',
+    'dFielNm': '%EC%9E%90%EB%A3%8C+%EC%A1%B0%ED%9A%8C',
     'excelSnsNos': '',
-    'pMENU_NO': 571,
-    'page': '',
-    'type_date': 9,
-    'ATTR_2': 202204, # LAST DATE
-    'ATTR_3': date, # NOW DATE 
-    'ATTR_7': 'DAY',
-    'querySn': 9,
-    'gubun': 'Search2',
-    'befor': 9,
-    'query': 'SELECT WQMN_CODE, WQMN_NM, CHCK_DE, TME, IEM_WTRTP FROM V_MSR_WQMN_DAY A',
-    'excelQuery': 'SELECT WQMN_CODE, WQMN_NM, CHCK_DE, TME, IEM_WTRTP FROM V_MSR_WQMN_DAY A',
-    'queryHeader': '',
-    'code': 'S001',
-    'station_code': '1018A50',
-    'serchRadio': 'A',
-    'station_codes': '1018A50',
-    'station_iem': 'IEM_WTRTP',
-    'station_iem': 'IEM_DPWT',
-    'station_iem': 'IEM_WTRTP',
-    'station_iem': 'IEM_DOC',
-    'station_iem': 'IEM_BOD',
-    'station_iem': 'IEM_COD',
-    'station_iem': 'IEM_SS',
-    'station_iem': 'IEM_TN',
-    'station_iem': 'IEM_TP',
-    'station_iem': 'IEM_TOC',
-    'station_iem': 'IEM_WTRTP',
-    'station_iem': 'IEM_WTRTP',
+    'pMENU_NO': 574,
+    'page': 1,
+    'ATTR_2': date,
+    'ATTR_3': date,
+    'querySn': 60,
+    'queryTp': 'autoMe',
+    'confirmNum': 2,
+    'station_code': 'S01004',
+    'pStartDay': dateFormat,
+    'pEndDay': dateFormat,
     'pageSize': 10
   }
 
@@ -50,25 +40,19 @@ def getTemp(date):
     'Content-Type' : 'application/x-www-form-urlencoded',
   }
 
-  data = requests.post(
+  result = requests.post(
     url, params=params, headers=headers
-  )
-
-  return data.json()['list'][0]
-
-def lambda_handler(event, context):
-  dt = datetime.datetime.now(timezone("Asia/Seoul"))
-  now_date = dt.strftime('%Y%m')  
-  result = getTemp(now_date)
+  ).json()['list'][0]
 
   result_json = {
     'result' : 'OK',
     'temperature' : result['IEM_WTRTP'],
-    'target_date' : result['CHCK_DE'],
-    'now_date' : dt.strftime('%Y/%m/%d %H:%M:%S'),
+    'target_date' : result['MESURE_DT'],
+    'now_date' : now,
   }
 
   return result_json
 
 print (lambda_handler(0, 0))
+
         
