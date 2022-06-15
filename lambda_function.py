ab00cf154111e04d15bf576a2b2b52eda38b98e3
Curo-Dev <admin@curo.xyz>
2022-06-15 13:12:23 +0900

    feat: url 대응

--- lambda_function.py
+++ lambda_function.py
@@ -9,24 +9,40 @@ def getDate(dt):
   return dt.strftime('%Y-%m-%d')
 
 def getTemp(date):
-  url = "http://www.koreawqi.go.kr/wQDDRealTotalDataList_D.wq"
+  url = "http://water.nier.go.kr/web/waterMeasure/toastList"
 
   params = {
-    'item_id' : 'M69',
-    'action_type' : 'L',
-    'action_type_seq' : '1',
-    'auto_flag' : '',
-    'auto_site_id' : 'S01007',
-    'search_data_type' : '1',
-    'search_flag2' : '1',
-    'river_id' : 'R01',
-    'site_id' : '\'S01004\'',
-    'site_name' : '구리',
-    'search_interval' : 'HOUR',
-    'search_date_from' : date,
-    'search_date_to' : date,
-    'order_type_1' : 'MSR_DATE',
-    'order_type_2' : 'ASC',
+    'dFielNm': '자료 조회',
+    'excelSnsNos': '',
+    'pMENU_NO': 571,
+    'page': '',
+    'type_date': 9,
+    'ATTR_2': 202204, # LAST DATE
+    'ATTR_3': date, # NOW DATE 
+    'ATTR_7': 'DAY',
+    'querySn': 9,
+    'gubun': 'Search2',
+    'befor': 9,
+    'query': 'SELECT WQMN_CODE, WQMN_NM, CHCK_DE, TME, IEM_WTRTP FROM V_MSR_WQMN_DAY A',
+    'excelQuery': 'SELECT WQMN_CODE, WQMN_NM, CHCK_DE, TME, IEM_WTRTP FROM V_MSR_WQMN_DAY A',
+    'queryHeader': '',
+    'code': 'S001',
+    'station_code': '1018A50',
+    'serchRadio': 'A',
+    'station_codes': '1018A50',
+    'station_iem': 'IEM_WTRTP',
+    'station_iem': 'IEM_DPWT',
+    'station_iem': 'IEM_WTRTP',
+    'station_iem': 'IEM_DOC',
+    'station_iem': 'IEM_BOD',
+    'station_iem': 'IEM_COD',
+    'station_iem': 'IEM_SS',
+    'station_iem': 'IEM_TN',
+    'station_iem': 'IEM_TP',
+    'station_iem': 'IEM_TOC',
+    'station_iem': 'IEM_WTRTP',
+    'station_iem': 'IEM_WTRTP',
+    'pageSize': 10
   }
 
   headers = {
@@ -34,49 +50,23 @@ def getTemp(date):
     'Content-Type' : 'application/x-www-form-urlencoded',
   }
 
-  data = requests.get(
+  data = requests.post(
     url, params=params, headers=headers
   )
 
-  soup = BeautifulSoup(
-    data.text,
-    'html.parser'
-  )
-
-  result = soup.find('table', { 'class', 'table_04'}).find_all_next('td')
-
-  return result[-1].text.strip()
+  return data.json()['list'][0]
 
 def lambda_handler(event, context):
   dt = datetime.datetime.now(timezone("Asia/Seoul"))
-  now_date = dt.strftime('%Y-%m-%d %H:%M:%S')  
-  result = getTemp(getDate(dt))
+  now_date = dt.strftime('%Y%m')  
+  result = getTemp(now_date)
 
-  if result == '':
-    if dt.hour <= 1:
-      result = getTemp(getDate(dt - datetime.timedelta(days = 1)))
-      if result == '':      
-        result_json = {
-          'result' : 'OK',
-          'tem' : result,
-          'date' : now_date,
-        }
-      else:
-        result_json = {
-          'result' : 'NO_CONTENT',    
-          'date' : now_date,
-        }    
-    else:
-      result_json = {
-        'result' : 'NO_CONTENT',    
-        'date' : now_date,
-      }
-  else:
-    result_json = {
-      'result' : 'OK',
-      'tem' : result,
-      'date' : now_date,
-    }
+  result_json = {
+    'result' : 'OK',
+    'temperature' : result['IEM_WTRTP'],
+    'target_date' : result['CHCK_DE'],
+    'now_date' : dt.strftime('%Y/%m/%d %H:%M:%S'),
+  }
 
   return result_json
 
